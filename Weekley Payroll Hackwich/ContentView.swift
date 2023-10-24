44a1f567866571791dd8ffe3e42e1a67fd9fe6dc
Umesh Sarma <usarma1200@gmail.com>
2023-10-24 09:43:42 -0500

    Initial Commit

---
+++ Weekley Payroll Hackwich/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Weekley Payroll Hackwich
+//
+//  Created by Umesh Sarma on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var hourlyWageTextField = ""
+
+        @State private var hoursWorkedTextField = ""
+
+        @State private var pay = 0.0
+
+    var body: some View {
+        VStack {
+            CustomText(text: "Weekly Payroll")
+            CustomTextField(placeholder: "Hourly Wage", variable: $hourlyWageTextField)
+            CustomTextField(placeholder: "Hours Worked", variable: $hoursWorkedTextField)
+            Button("Calculate")
+            {
+                if let hourlyWage = Double(hourlyWageTextField) {
+                    if let hoursWorked = Double(hoursWorkedTextField) {
+                        pay = calculateGrossPay(hourlyWage: hourlyWage, hoursWorked: hoursWorked)
+                    }
+                }
+                
+            }
+            CustomText(text: "Gross Pay")
+            CustomText(text: "$" + String(format: "%.2f",pay))
+            
+        }
+        .padding()
+    }
+    
+    func calculateGrossPay(hourlyWage: Double, hoursWorked: Double) -> Double {
+        if hoursWorked > 40 {
+            let regularPay = hourlyWage * 40
+            let overTimePay = hourlyWage * (hoursWorked - 40) * 1.5
+            return regularPay + overTimePay
+        }
+            return hourlyWage * hoursWorked
+
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CustomTextField: View {
+
+    let placeholder : String
+
+    let variable : Binding<String>
+
+    var body: some View {
+
+        TextField(placeholder, text: variable)
+
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            .multilineTextAlignment(.center)
+
+            .frame(width: 200, height: 30, alignment: .center)
+
+            .font(.body)
+
+            .padding()
+
+    }
+
+}
+
+struct CustomText: View {
+
+    let text : String
+
+    var body: some View {
+
+        Text(text)
+
+            .font(.title)
+
+            .fontWeight(.bold)
+
+    }
+
+}
---
+++ Weekley Payroll Hackwich/Weekley_Payroll_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekley_Payroll_HackwichApp.swift
+//  Weekley Payroll Hackwich
+//
+//  Created by Umesh Sarma on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Weekley_Payroll_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
